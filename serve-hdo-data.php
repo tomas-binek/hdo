d85380615f79f9a62468aee3be998ac05e32552d
Tomáš Binek <tomasbinek@seznam.cz>
2023-06-04 15:46:03 +0200

    First working version

--- serve-hdo-data.php
+++ serve-hdo-data.php
@@ -1,4 +1,12 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+#die(posix_getpwuid(posix_geteuid())['name']);
+
+$parserScript = $_SERVER['DOCUMENT_ROOT'] . "/hdo-data.py";
+$cacheDir = $_SERVER['DOCUMENT_ROOT'] . "/cache";
 
 function serve(int $httpCode, $content, $contentType = 'text/plain')
 {
@@ -6,12 +14,49 @@ function serve(int $httpCode, $content, $contentType = 'text/plain')
     header('Content-Type: ' . $contentType);
     echo $content;
     die();
-
 }
 
-if (! array_key_exists('command'), $_GET)
+function getCacheFilePath(int $command, int $days)
+{
+    global $cacheDir;
+    return $cacheDir . '/command:'. $command . ',since:' . date('Y-m-d') . ',days:' . $days . '.txt';
+}
+
+if (! array_key_exists('command', $_GET))
     serve(400, "Missing 'command' argument");
-else if (! array_key_exists('days'), $_GET)
+else if (! array_key_exists('days', $_GET))
     serve(400, "Missing 'days' argument");
 else
-    serve(200, ':-)')
+{
+    $arg_command = intval($_GET['command']);
+    $arg_days = intval($_GET['days']);
+    $cacheFile = getCacheFilePath($arg_command, $arg_days);
+
+    if (! file_exists($cacheFile))
+    {
+        exec("/usr/bin/python3 $parserScript $arg_command $arg_days", $pythonOutput, $pythonExitCode);
+
+        //print_r($_SERVER);
+        if ($pythonExitCode != 0)
+            serve(500, "Python died (" . $pythonExitCode . ")");
+        else
+        {
+            file_put_contents(
+                $cacheFile,
+                json_encode(
+                    array_map(
+                        function ($line)
+                        {
+                            $lineAsObject = new stdClass();
+                            list($lineAsObject->start, $lineAsObject->end, $lineAsObject->tariff) = explode(' ', $line);
+                            return $lineAsObject;
+                        },
+                        $pythonOutput
+                    )
+                )
+            );
+        }
+    }
+
+    serve(200, file_get_contents($cacheFile), 'application/json');
+}
